41b993659ddffa95bc540bdf16f94f35c873cc1e
ToyeshShende <119116915+ToyeshShende@users.noreply.github.com>
2023-06-22 14:19:37 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -14,7 +14,7 @@ export const Banner = () => {
            
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>"Hi! I'm Toyesh Shende" </h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>I am final year student at VIIT,Pune and pursuing computer engineering. I am interest in full stack development and currently learning it and trying to implement mern stack on projects. Also I liked to solve questions of dsa in cpp which helps me to imrove my problem solving and critical thinking skills. About the competative coding I have 1623 rating on CodeChef.Other than study ,i like to play cricket on my free time.</p>
 
           </Col>
           <Col xs={12} md={6} xl={5}>
